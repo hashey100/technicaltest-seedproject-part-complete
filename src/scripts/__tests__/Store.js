98f96030a7d471484bc8649c7cd459b234ed4ade
hesham.saied <hesham.saied@itech.media>
2021-01-16 10:56:33 +0000

    Test setup with store + Base tests created

--- src/scripts/__tests__/Store.js
+++ src/scripts/__tests__/Store.js
@@ -1,16 +1,52 @@
 import Store from "../Store";
 import mockData from "../../../public/db.json";
 
+/**
+ * Tests for the following criteria
+ * 1. No filters applied should show 11 deals
+ * 2. Broadband selected should show 4 broadband deals
+ * 3. Broadband & TV selected should show 1 deal
+ * 4. Sky selected should show 1 sky deal
+ * 5. BT Broadband and TV selected should show 2 deals with BT broadband and tv only
+ */
+
 describe("filter", () => {
-  it("should return all deals when no filters applied", () => {
-    // Arrange
-    const sut = new Store();
-    sut.setDeals(mockData.deals);
 
-    // Act
-    const result = sut.deals;
+  let store = new Store();
 
-    // Assert
-    expect(result).toEqual(mockData.deals);
+  /**
+   * Initiating beforeEach so I can set the store value with the mockData
+   * before any of the tests have ran
+  */
+
+  beforeEach(() => {
+    store = new Store();
+    store.setDeals(mockData.deals)
   });
+
+   /**
+   * Running afterEach which will clear the store after a test has ran this follows good practice
+  */
+
+  afterEach(() => {
+    store.setDeals()
+  });
+
+  // Setting up the base tests which covers the functional requirements
+
+  it("should return all deals when no filters applied", () => {
+  });
+
+  it("should return 4 broadband deals when broadband selected", () => {
+  });
+
+  it("should return 1 deals when broadband and TV selected", () => {
+  });
+
+  it("should return 1 sky deal when sky is selected", () => {
+  });
+
+  it("should return 2 deals when BT broadband and TV is selected", () => {
+  });
+
 });
